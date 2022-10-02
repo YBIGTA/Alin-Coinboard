5d98942e35fe966b974d8ceeb4419c19aefefb1b
Hyeokjun seo <75557859+ANTARES-KOR@users.noreply.github.com>
2022-10-03 03:02:14 +0900

    [Chore] 코인 정보 프로듀서 개발환경 셋업 (#3)
    * [Chore] 도커파일 작성 시도 - 실패
    
    * [Feat] 카프카 클러스터 개발환경 설정 및 프로듀서 코드 빌드
    
    * [Docs] 개발환경 셋업 방법 설명

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 # Alin-Coinboard
+
 비트코인 거래량 / 김치 프리미엄 등 시각화 대시보드
 
 ## 팀원
+
 - 20기 백승연
 - 20기 서혁준
 - 20기 이학민
 - 20기 주현경
 
 ## 목표
+
 - BTC, BNB, XRP, ETH, DOGE, TRX 코인에 대해서 현재가, 가격 변동 추이, 김치 프리미엄, 거래량 급등 정보를 보여주는 대시보드 구축
 
 ## 프로젝트 파이프라인
+
 ```mermaid
 graph LR;
 
@@ -62,4 +66,33 @@ T_B --> S3
 T_U --> S3
 T_Kim --> S3
 T_Vol --> S3
+```
+
+## 개발환경 셋업 방법
+
+1. `docker-compose` 를 이용한 카프카 개발환경 실행
+
+```bash
+
+# 카프카 클러스터 실행
+docker compose up -d
+
+# binance 토픽 데이터 조회 (kafka-console-consumer)
+docker compose logs binance-consumer
+
+# upbit 토픽 데이터 조회 (kafka-console-consumer)
+docker compose logs upbit-consumer
+```
+
+2. producer 코드 실행
+
+```bash
+# Intel Mac 환경에서 실행
+./producer/bin/start-producer-darwin-amd64
+
+# Apple Silicon Mac 환경에서 실행
+./producer/bin/start-producer-darwin-arm64
+
+# 윈도우즈 환경에서 실행
+start ./producer/bin/start-producer-windows-amd64.exe
 ```
--- producer/cmd/main.go
+++ producer/cmd/main.go
@@ -4,7 +4,6 @@ import (
 	"alin-coinboard/producer/pkg/upbit"
 	"encoding/json"
 	"fmt"
-	"io"
 	"os"
 	"sync"
 
@@ -25,21 +24,16 @@ func checkError(err error) {
 
 // 주어진 symbol들에 대한 Binance API Kline 데이터를 웹소켓으로 받아와 /data/binance.txt에 저장
 // https://binance-docs.github.io/apidocs/spot/en/#kline-candlestick-streams
-func save_binance_kline(tickers []string, interval string) {
+func produce_binance_kline(tickers []string, interval string) {
 	errHandler := func(err error) {
 		fmt.Println(err)
 	}
 	wsKlineHandler := func(event *binance.WsKlineEvent) {
-		file, err := os.OpenFile("../data/binance.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-		checkError(err)
 
 		jsonEvent, err := json.Marshal(event)
 		checkError(err)
 
-		_, err = io.WriteString(file, string(jsonEvent) + "\n")
-		checkError(err)
-
-		// produceMessageToCluster(binanceTopic, string(jsonEvent))
+		produceMessageToCluster(binanceTopic, string(jsonEvent))
 	}
 
 	symbolIntervalPair := make(map[string]string)
@@ -57,22 +51,16 @@ func save_binance_kline(tickers []string, interval string) {
 
 // 주어진 symbol들에 대한 Upbit API Ticker 데이터를 웹소켓으로 받아와 /data/upbit.txt에 저장
 // https://docs.upbit.com/docs/upbit-quotation-websocket#현재가ticker-응답
-func save_upbit_ticker(tickers []string) {
+func produce_upbit_ticker(tickers []string) {
 	errHandler := func (err error)  {
 		fmt.Println(err)	
 	}
 	wsTickerHandler := func(event *upbit.WsSimpleTicker) {
-			file, err := os.OpenFile("../data/upbit.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-			if err != nil {
-					fmt.Println(err)
-					return
-			}
+
 			jsonEvent, err := json.Marshal(event)
 			checkError(err)
-			_, err = io.WriteString(file, string(jsonEvent) + "\n")
-			checkError(err)
 
-			// produceMessageToCluster(upbitTopic, string(jsonEvent))
+			produceMessageToCluster(upbitTopic, string(jsonEvent))
 	}
 	doneC, _, err := upbit.WsCombinedTickerServe(tickers, wsTickerHandler, errHandler)
 	if err != nil {
@@ -89,8 +77,8 @@ func main() {
 	var wait sync.WaitGroup
 	wait.Add(2)
 
-	go save_binance_kline([]string{"btcusdt", "ethusdt"}, "1s")
-	go save_upbit_ticker([]string{"KRW-BTC", "KRW-ETH"})
+	go produce_binance_kline([]string{"btcusdt", "ethusdt"}, "1s")
+	go produce_upbit_ticker([]string{"KRW-BTC", "KRW-ETH"})
 
 	wait.Wait()
 
