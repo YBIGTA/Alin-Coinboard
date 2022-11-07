adb883985ca428c0e9d7d057f2f46564adfc9b8a
이학민 <lhmlhm1111@hakminui-MacBookAir.local>
2022-11-07 15:02:34 +0900

    TradingVolume_add

---
+++ streams/src/main/java/tradingvolume/TradingVolumeUtil.java
@@ -0,0 +1,21 @@
+package tradingvolume;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TradingVolumeUtil {
+    public static String changeTimestampToDateString(String timestamp){
+        Date date = new Date(Long.valueOf(timestamp));
+        SimpleDateFormat sdf = new SimpleDateFormat ("yyyy-MM-dd kk:mm:ss");
+        return sdf.format(date);
+    }
+
+    public static String getValue(String keyValue, boolean isQuoted){
+        String value = keyValue.split(":")[1];
+        if (isQuoted){
+            value = value.substring(1, value.length() - 1);
+        }
+        return value;
+    }
+
+}
