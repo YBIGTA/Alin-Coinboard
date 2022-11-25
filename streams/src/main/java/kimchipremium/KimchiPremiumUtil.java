663bc007dfe72cdf00b3948d7fc8c705a22dacd4
hyunkyungju <wngusrudrid@naver.com>
2022-11-26 04:05:51 +0900

    [Fix] trading volume 데이터 형식 오류 수정

--- streams/src/main/java/kimchipremium/KimchiPremiumUtil.java
+++ streams/src/main/java/kimchipremium/KimchiPremiumUtil.java
@@ -2,14 +2,15 @@ package kimchipremium;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 import java.util.TimeZone;
 
 public class KimchiPremiumUtil {
     public static String changeTimestampToDateString(Long timestamp){
-        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 
         Date date = new Date(timestamp);
-        SimpleDateFormat sdf = new SimpleDateFormat ("yyyy-MM-dd kk:mm:ss");
+        SimpleDateFormat sdf = new SimpleDateFormat ("yyyy-MM-dd kk:mm:ss", Locale.KOREA);
+        sdf.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
         return sdf.format(date);
     }
 }
